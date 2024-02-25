a17207cdd30eefad856626941369056355112865
Parth Sharma <sharmaparth1208@gmail.com>
2024-02-25 12:00:11 +0530

    first commit

--- src/test/java/com/example/spe_task/SpeTaskApplicationTest.java
+++ src/test/java/com/example/spe_task/SpeTaskApplicationTest.java
@@ -8,46 +8,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpeTaskApplicationTests {
     @Test
     public void testAdd() {
-        double result = SpeTaskApplication.add(3, 4);
-        assertEquals(7, result, 0.0001,"testing addition of two numbers:"); // Delta is used for double comparison
+        double result = SpeTaskApplication.add(9, 4);
+        assertEquals(13, result, 0.0001,"testing addition of two numbers:"); // Delta is used for double comparison
     }
 
     @Test
     public void testSubtract() {
-        double result = SpeTaskApplication.subtract(8, 5);
+        double result = SpeTaskApplication.subtract(5, 2);
         assertEquals(3, result, 0.0001,"testing subtraction of two numbers");
     }
 
     @Test
     public void testMultiply() {
-        double result = SpeTaskApplication.multiply(2, 6);
-        assertEquals(12, result, 0.0001,"testing multiplication of two numbers");
+        double result = SpeTaskApplication.multiply(6, 4);
+        assertEquals(24, result, 0.0001,"testing multiplication of two numbers");
     }
 
     @Test
     public void testDivide() {
-        double result = SpeTaskApplication.division(9, 3);
-        assertEquals(3, result, 0.0001,"testing division of two numbers of two numbers");
+        double result = SpeTaskApplication.division(30, 15);
+        assertEquals(2, result, 0.0001,"testing division of two numbers of two numbers");
     }
 
 
 
     @Test
     public void testSquareRoot() {
-        double result = SpeTaskApplication.root(25);
-        assertEquals(5, result, 0.0001,"testing square root of a number");
+        double result = SpeTaskApplication.root(400);
+        assertEquals(20, result, 0.0001,"testing square root of a number");
     }
 
-//    @Test
-//    public void testNaturalLog() {
-//        double result = SpeTaskApplication.naturalLog(1);
-//        assertEquals(0, result, 0.0001,"testing natural log of a number");
-//    }
 
     @Test
     public void testFactorial() {
-        double result = SpeTaskApplication.factorial(5);
-        assertEquals(120, result,"testing factorial of a number");
+        double result = SpeTaskApplication.factorial(6);
+        assertEquals(720, result,"testing factorial of a number");
     }
 
     @Test
